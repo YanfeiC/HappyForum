b48900f5be48191f245d39da06ad331847877dbf
cyf <cs008007009@sina.com>
2017-03-08 21:15:34 +0800

    delete thymeleaf config
    spring auto config thymeleaf,do not need to config

--- src/main/java/space/shadowc/HappyforumApplication.java
+++ src/main/java/space/shadowc/HappyforumApplication.java
@@ -2,38 +2,9 @@ package space.shadowc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.servlet.ViewResolver;
-import org.thymeleaf.TemplateEngine;
-import org.thymeleaf.spring4.SpringTemplateEngine;
-import org.thymeleaf.spring4.templateresolver.SpringResourceTemplateResolver;
-import org.thymeleaf.spring4.view.ThymeleafViewResolver;
 
 @SpringBootApplication
 public class HappyforumApplication {
-    @Bean
-    public SpringResourceTemplateResolver templateResolver() {
-        SpringResourceTemplateResolver templateResolver = new SpringResourceTemplateResolver();
-        templateResolver.setPrefix("/resources/templates/");
-        templateResolver.setSuffix(".html");
-        templateResolver.setTemplateMode("HTML5");
-        templateResolver.setCacheable(true);
-        return templateResolver;
-    }
-
-    @Bean
-    public TemplateEngine templateEngine() {
-        SpringTemplateEngine templateEngine = new SpringTemplateEngine();
-        templateEngine.setTemplateResolver(templateResolver());
-        return templateEngine;
-    }
-
-    @Bean
-    public ViewResolver viewResolver(SpringTemplateEngine templateEngine){
-        ThymeleafViewResolver viewResolver = new ThymeleafViewResolver();
-        viewResolver.setTemplateEngine(templateEngine);
-        return viewResolver;
-    }
 
     public static void main(String[] args) {
         SpringApplication.run(HappyforumApplication.class, args);
